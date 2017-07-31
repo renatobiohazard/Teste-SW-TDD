58e9419214259afb876fdc8ae2c674cdbeb92eb7
Luiz Claudio <20142014040248@gitlab.devops.ifrn.edu.br>
2017-07-31 09:19:33 -0300

    Criando teste para segundo caso (ano % 100 != 0))

--- spec/testeBissexto.spec.js
+++ spec/testeBissexto.spec.js
@@ -3,11 +3,17 @@ var teste = require('../app.js');
 
 describe("O ano não é bissexto pois", function() {
 
+  // PRIMEIRO CASO
   it("não é divisível por 4.", function() {
     expect(true).toBe(teste.ehBissexto(4));
   });
   it("é divisível por 4.", function() {
     expect(false).toBe(teste.ehBissexto(5));
+  });
+
+  // SEGUNDO CASO
+  it("não é divisível por 100.", function() {
+    expect(false).toBe(teste.ehBissexto(1400));
   });
 
 });
